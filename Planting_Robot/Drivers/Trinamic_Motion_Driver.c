6ed7e12389552e97ad3ef978ed3ac379237c2ad4
Patrick Rossacher <patrick.rossacher@stud.hslu.ch>
2017-05-23 17:44:36 +0200

    implemented motor test program

--- Planting_Robot/Drivers/Trinamic_Motion_Driver.c
+++ Planting_Robot/Drivers/Trinamic_Motion_Driver.c
@@ -7,35 +7,85 @@
 
 #include "Trinamic_Motion_Driver.h"
 
+#define TARGET_ADDRESS 		0x01
+#define MOTOR				0x00
 
 static void Trinamic_Motion_Task(void *pvParameters) {
 	(void)pvParameters; /* parameter not used */
 
+	unsigned char value_slow[] = {0x00, 0x00, 0x01, 0x5E};
+	unsigned char value_fast[] = {0x00, 0x00, 0x02, 0xBC};
+	unsigned char value_stop[] = {0x00, 0x00, 0x00, 0x00};
+	unsigned char value_pos1[] = {0x00, 0x00, 0x02, 0xBC};
+
+
 	for(;;) {
-		LED2_Neg();
-		FRTOS1_vTaskDelay(1000/portTICK_RATE_MS);
+#if 0
+		Trinamic_Motion_Test();
+#endif
+		Trinamic_Motion_sendPacket(MVP, 0x01, value_pos1);
+		FRTOS1_vTaskDelay(5000/portTICK_RATE_MS);
 	}
 }
 
-/*
-//Checksum calculation
-unsigned char i, Checksum;
-unsigned char Command[9];
-//Set the “Command” array to the desired command
-Checksum = Command[0];
-for(i=1; i<8; i++)
-Checksum+=Command[i];
-Command[8]=Checksum; //insert checksum as last byte of the command
-//Now, send the command back to the module
-*/
+void Trinamic_Motion_Test(){
+	unsigned char value[] 	   = {0x00, 0x00, 0x00, 0x00};
+	int i;
+
+	for(i=0;i<350;i+=10){
+		value[0] = (i>>24) 	& 0xff;
+		value[1] = (i>>16) 	& 0xff;
+		value[2] = (i>>8) 	& 0xff;
+		value[3] = (i>>0) 	& 0xff;
+		Trinamic_Motion_sendPacket(ROR, 0x00, value);
+		FRTOS1_vTaskDelay(100/portTICK_RATE_MS);
+
+	}
+	for(i=350;i>0;i-=10){
+		value[0] = (i>>24) 	& 0xff;
+		value[1] = (i>>16) 	& 0xff;
+		value[2] = (i>>8) 	& 0xff;
+		value[3] = (i>>0) 	& 0xff;
+		Trinamic_Motion_sendPacket(ROR, 0x00, value);
+		FRTOS1_vTaskDelay(100/portTICK_RATE_MS);
+	}
+}
+
 
 /*
- * Sends a char packet to the Driver
+ * Sends a char packet to the Driver, wrapping it up with the
+ * Target Address and the checksum. The Command Format always
+ * consists out of 9 Bytes starting with the Modul address and
+ * ending with the calculated Checksum.
  */
-void Trinamic_Motion_sendPacket(unsigned char packet[], int packetlength){
-		for(int i=0; i<packetlength; i++){
-			CLS1_SendChar(packet[i]);
-		}
+void Trinamic_Motion_sendPacket(trinamic_command_t instruction, unsigned char type, unsigned char value[]){
+	unsigned char command[9];
+	unsigned char checksum = 0;
+
+	command[0] = TARGET_ADDRESS;
+	command[1] = instruction;
+	command[2] = type;
+	command[3] = MOTOR;
+	command[4] = value[0];
+	command[5] = value[1];
+	command[6] = value[2];
+	command[7] = value[3];
+
+	/*
+	 * calculating the checksum: Adding up all
+	 * commands including the module address byte
+	 */
+	for(int i=0; i<8; i++){
+		checksum += command[i];
+	}
+	command[8] = checksum;
+
+	/*
+	 * send the command packet
+	 */
+	for(int i=0; i<=8; i++){
+		CLS3_SendChar(command[i]);
+	}
 }
 
 void Trinamic_Motion_Driver_Init(void){
--- Planting_Robot/Drivers/Trinamic_Motion_Driver.h
+++ Planting_Robot/Drivers/Trinamic_Motion_Driver.h
@@ -12,10 +12,35 @@
 #include "FRTOS1.h"
 #include "LED2.h"
 #include "Stop_IN.h"
-//#include "CLS1.h"		//add pe component
+#include "CLS3.h"
+
+typedef enum TRINAMIC_COMMAND_{
+	ROR			= 1,		// rotate right
+	ROL			= 2,		// rotate left
+	MST			= 3,		// motor stop
+	MVP 		= 4,		// move to position
+
+	SAP 		= 5,		// set axis parameter
+	GAP 		= 6,		// get axis parameter
+	STAP 		= 7,		// store axis parameter
+	RSAP 		= 8,		// restore axis parameter
+	SGP			= 9,		// set global parameter
+	GGP			= 10,		// get global parameter
+	STGP		= 11,		// store global parameter
+	RSGP		= 12,		// restore global parameter
+
+	SIO			= 14,		// set output
+	GIO			= 15,		// get input/output
+
+	CALC		= 19,		// calculate
+	COMP		= 20,		// compare
+	JC			= 21,		// jump conditional
+} trinamic_command_t;
 
 static void Trinamic_Motion_Task(void *pvParameters);
 
 void Trinamic_Motion_Driver_Init(void);
+
+void Trinamic_Motion_sendPacket(unsigned char instruction, unsigned char type, unsigned char value[]);
 
 #endif /* DRIVERS_Trinamic_Motion_DRIVER_H_ */
